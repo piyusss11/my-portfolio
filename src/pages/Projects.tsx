ab469cbb4b8030bc1f911fab5f45630a5a4726ef
piyusss11 <piyusss11@gmail.com>
2025-01-30 12:57:16 +0530

    3rd atttempt

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,70 +1,11 @@
-import { useEffect, useRef } from "react";
 import { MoreProjectsIcon } from "../components/Icons";
 import ProjectBox from "../components/ProjectBox";
 import { projectsInfo } from "../utils/projectsInfo";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 const Projects = () => {
-  const container = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-  }, []);
-  useGSAP(() => {
-    gsap.utils
-      .toArray(["#project-1", "#project-2", "#project-3", "#project-4"])
-      .forEach((el: any) => {
-        gsap.from(el, {
-          x: -200,
-          opacity: 0,
-          duration: 0.6,
-          stagger: 0.2,
-          ease: "power2.out",
-          scrollTrigger: {
-            trigger: container.current,
-            start: "top 70%", 
-            end: "top 40%",
-            toggleActions: "play reverse play reverse",
-            onEnter: () => {
-              gsap.to(el, { x: 0, opacity: 1, duration: 0.6 });
-            },
-            onLeave: () => {},
-            onEnterBack: () => {
-              gsap.to(el, { x: 0, opacity: 1, duration: 0.6 });
-            },
-            onLeaveBack: () => {
-              gsap.set(el, { x: -200, opacity: 0 }); 
-            },
-          },
-        });
-      });
-    gsap.utils
-      .toArray(["#project-5", "#project-6", "#project-7", "#project-8"])
-      .forEach((el: any) => {
-        gsap.from(el, {
-          x: 200,
-          opacity: 0,
-          duration: 0.6,
-          stagger: 0.2,
-          ease: "power2.out",
-          scrollTrigger: {
-            trigger: container.current,
-            start: "top 70%", 
-            end: "top 40%",
-            toggleActions: "play reverse play reverse",
-            onEnter: () => {
-              gsap.to(el, { x: 0, opacity: 1, duration: 0.6 });
-            },
-            onEnterBack: () => {
-              gsap.to(el, { x: 0, opacity: 1, duration: 0.6 });
-            },
-          },
-        });
-      });
-  });
+  
+  
   return (
-    <section ref={container} className="panel h-screen snap-start relative">
+    <section className="panel h-screen snap-start relative">
       <div className="shape absolute w-full h-[40vh] bottom-0 left-0 right-0"></div>
       <div className="backdrop-blur-3xl flex flex-col items-center md:block px-14 md:px-30 py-16 h-full w-full">
         <div className="md:flex md:justify-between md:items-center space-y-6 md:space-y-0 mt-4 ">
